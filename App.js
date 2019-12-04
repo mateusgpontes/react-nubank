d2aa8623ad350a3182b8e8c130aa5d5445814ba3
mateusgpontes <mateus.gpontes@hotmail.com>
2019-12-03 22:03:44 -0300

    Create Headers

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import './src/config/ReactotronConfig';
 
 import Routes from './src/routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8a05be" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 7% 0% 5%;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 2.5%;
+`;
+
+export const Logo = styled.Image``;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+  margin-left: 5%;
+`;
