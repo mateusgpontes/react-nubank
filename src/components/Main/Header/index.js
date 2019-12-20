9712af6e717e13a4cf41901f403d40ed9158e1ed
mateusgpontes <mateus.gpontes@hotmail.com>
2019-12-20 14:59:53 -0300

    move components

--- src/components/Main/Header/index.js
+++ src/components/Main/Header/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import {Container, Top, Logo, Title} from './styled';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import logo from '../../assets/Nubank_Logo.png';
+import logo from '../../../assets/Nubank_Logo.png';
+
+import {Container, Top, Logo, Title} from './styled';
 
 export default function Header() {
   return (
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from '../../components/Header';
-import Tabs from '../../components/Tabs';
+import Header from '../../components/Main/Header';
+import Tabs from '../../components/Main/Tabs';
 
 import {Container} from './styled';
 
